f0479ec7537a70298c1457a94925c32cc97c3907
Kaimite <plhubert@kaimite.net>
2011-07-06 10:35:26 +0200

    Example of finding the names of uploaded files

---
+++ example/save.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<!--
+/*
+ * jQuery File Upload Plugin HTML Example 5.0.5
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ */
+-->
+<html lang="en" class="no-js">
+<head>
+<meta charset="utf-8">
+<title>jQuery File Upload Example</title>
+<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/base/jquery-ui.css" id="theme">
+<link rel="stylesheet" href="../jquery.fileupload-ui.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Form values</h1>
+<pre>
+	<?php print_r($_POST); ?>
+</pre>
+</body> 
+</html>
